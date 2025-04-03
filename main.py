5d6ab32901ed1e4098a9965cc89a29d10e73be0c
ShivekSinghal <singhalshivek24@gmail.com>
2025-04-04 02:51:34 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify, flash, session
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4910)
